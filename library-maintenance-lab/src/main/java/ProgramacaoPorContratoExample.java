24174726e7f72862ef0afaeb05a124e14edf1d5f
Goku - João Vitor dos Santos <qakarotto@gmail.com>
2026-05-14 17:51:33 -0300

    feat: ajustando exemplo de Contrato para incluir método main

--- library-maintenance-lab/src/main/java/ProgramacaoPorContratoExample.java
+++ library-maintenance-lab/src/main/java/ProgramacaoPorContratoExample.java
@@ -1,5 +1,10 @@
 public class ProgramacaoPorContratoExample {
 
+    public static void main(String[] args) {
+        ProgramacaoPorContratoExample example = new ProgramacaoPorContratoExample();
+        example.emprestarLivro(1, 1, 14);
+    }
+
     public void emprestarLivro(int usuarioId, int livroId, int maxDias) {
         assert usuarioId > 0 : "Usuário deve ser informado";
         assert livroId > 0 : "Livro deve ser informado";
